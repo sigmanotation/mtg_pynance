6bb4de7a6922c8661d5721b3cc2e15a12834be27
Zach Serikow <76140451+sigmanotation@users.noreply.github.com>
2024-06-08 13:23:29 -0400

    WIP.

--- src/mtg_pynance/analysis.py
+++ src/mtg_pynance/analysis.py
@@ -11,15 +11,24 @@ def collection_value(database_path: Path):
     cursor: sqlite3.Cursor = connection.cursor()
 
     # Calculate market value of all cards
-    sql_command = "select cid from purchase_price"
+    sql_command = "select * from purchase_price"
     result = cursor.execute(sql_command).fetchall()
 
-    mya = np.array([cid[0] for cid in result])
-    # ppdict = {cid: price for cid, price in result}
-    # result = cursor.execute(sql_command).fetchone()
-    # for x in result:
-    #     print(x)
+    card_id_db = np.array([cid for cid in result])
+    mydict = {}
+    for cid, price in enumerate(card_id_db):
+        sql_command = f"select * from card_{cid}"
+        result = cursor.execute(sql_command).fetchall()
+        combined = {key: mydict.get(key, 0) + new.get(key, 0) for key in set(old) | set(new)} 
+        mydict = {asdfasdf for timestamp in set(mydict) | set}
+
+
     # connection.close()
+
+
+def wee(cid, dicty):
+
+
 
 
 def pull(database_path):
--- src/mtg_pynance/run.py
+++ src/mtg_pynance/run.py
@@ -11,9 +11,9 @@ collection_path = Path("/Users/zachserikow/Desktop/collection.csv")
 
 config = Config(workspace_path, collection_path)
 
-run_mtg_pynance(config)
+# run_mtg_pynance(config)
 
-# collection_value(config.get_database_path())
-pull(config.get_database_path())
+collection_value(config.get_database_path())
+# pull(config.get_database_path())
 
 print("--- %s seconds ---" % (time.time() - start_time))
